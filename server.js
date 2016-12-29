d339f5d26b47d963757b319bbe8b60c1e9d37ffa
Minh Ta <Minh Ta>
2016-12-29 12:24:35 +1000

    change server.js

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var port = process.env.PORT || 8080;
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.get('/:timestamp', function (req, res) {
+    var dtString;
+    var timestamp = req.params.timestamp;
 
+    if (/^\d{10,}$/.test(timestamp)) {
+        dtString = moment(timestamp, 'X');
+    }
+    else {
+        dtString = moment(timestamp, 'MMMM D, YYYY');
+    }
+
+    if (dtString.isValid()) {
+        res.json({
+            'unix': dtString.format('X'),
+            'natural': dtString.format('MMMM D, YYYY')
+        })
+    }
+    else {
+        res.json({
+            'unix': null,
+            'natural': null
+        })
+    }
+});
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port + '!');
+});
