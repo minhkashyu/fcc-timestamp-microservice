476547e25f0c38c7267e38c851bd9702d4044ab6
Minh Ta <minh.ta@creativeintersection.com>
2024-01-06 13:50:36 +1000

    * Update API to pass fcc test cases * Add test cases to README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The solution link is [https://mks-fcc-timestamp-microservice.glitch.me](https://mks-fcc-timestamp-microservice.glitch.me).
 
-## Example Usage:
+## Example Usage
 
 [https://mks-fcc-timestamp-microservice.glitch.me/api/2015-12-25](https://mks-fcc-timestamp-microservice.glitch.me/api/2015-12-25)
 
@@ -11,3 +11,21 @@ The solution link is [https://mks-fcc-timestamp-microservice.glitch.me](https://
 ## Example Output
 
 `{"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"}`
+
+## Test Cases
+
+*   You should provide your own project, not the example URL.
+
+*   A request to `/api/:date?` with a valid date should return a JSON object with a `unix` key that is a Unix timestamp of the input date in milliseconds (as type Number)
+
+*   A request to `/api/:date?` with a valid date should return a JSON object with a `utc` key that is a string of the input date in the format: `Thu, 01 Jan 1970 00:00:00 GMT`
+
+*   A request to `/api/1451001600000` should return `{ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }`
+
+*   Your project can handle dates that can be successfully parsed by `new Date(date_string)`
+
+*   If the input date string is invalid, the API returns an object having the structure `{ error : "Invalid Date" }`
+
+*   An empty date parameter should return the current time in a JSON object with a `unix` key
+
+*   An empty date parameter should return the current time in a JSON object with a `utc` key
--- index.js
+++ index.js
@@ -21,21 +21,45 @@ app.get('/', function(req, res) {
 // [project url]/api/2015-12-25
 // [project url]/api/1451001600000
 // Output: {"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
-app.get('/api/:strDatetime', function(req, res) {
+app.get('/api/:reqDate?', function(req, res) {
   const isValidDate = (d) => {
     return d instanceof Date && !isNaN(d);
   };
 
-  let strDatetime = req.params.strDatetime || '';
-  if (parseInt(strDatetime) == strDatetime) {
-    strDatetime = parseInt(strDatetime);
+  // An empty date parameter should return the current time
+  // in a JSON object with a unix key
+
+  let reqDate = req.params.reqDate || '';
+  // A request to /api/1451001600000 should return
+  // { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+  if (parseInt(reqDate) == reqDate) {
+    reqDate = parseInt(reqDate);
   }
 
-  let dt = new Date(strDatetime);
-  if (!isValidDate(dt)) {
+  // Your project can handle dates that can be successfully
+  // parsed by new Date(date_string)
+  let dt = new Date(reqDate);
+  // An empty date parameter should return the current time
+  // in a JSON object with a utc key
+  if (!reqDate) {
     dt = new Date();
   }
 
+  // If the input date string is invalid, the API returns
+  // an object having the structure { error : "Invalid Date" }
+  if (!isValidDate(dt)) {
+    return res.json({
+      error: 'Invalid Date',
+    });
+  }
+
+  // A request to /api/:date? with a valid date should return
+  // a JSON object with a unix key that is a Unix timestamp
+  // of the input date in milliseconds (as type Number)
+
+  // A request to /api/:date? with a valid date should
+  // return a JSON object with a utc key that is a string of
+  // the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
   res.json({
     unix: dt.getTime(),
     utc: dt.toUTCString(),
